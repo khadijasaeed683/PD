d2f528b527319fa28258526a24a02b23160275bb
Khadija Saeed <144422229+khadijasaeed683@users.noreply.github.com>
2024-02-14 09:42:32 -0800

    Add files via upload

---
+++ PD3/Task6game/Task6game/bl/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6game
+{
+    internal class Enemy
+    {
+        public char symbol;
+        public int ex;
+        public int ey;
+        public Enemy(char symbol, int ex, int ey)
+        {
+            this.symbol = symbol;
+            this.ex = ex;
+            this.ey = ey;
+        }
+        public void MoveEnemy(int x, int y)
+        {
+            ex=x; ey=y;
+        } 
+        public void DisplayEnemy()
+        {
+            Console.SetCursorPosition(ex, ey);
+            Console.WriteLine(symbol);
+        }
+
+    }
+}
---
+++ PD3/Task6game/Task6game/bl/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6game
+{
+    internal class Player
+    {
+        public char symbol;
+        public int px;
+        public int py;
+        public Player(char symbol, int px, int py)
+        {
+            this.symbol = symbol;
+            this.px = px;
+            this.py = py;
+        }
+        public void MovePlayer(int x, int y)
+        {
+              px=x; py = y;
+        }
+        public void DisplayPlayer()
+        {
+            Console.SetCursorPosition(px, py);
+            Console.Write(symbol);s
+        }
+    }
+}
